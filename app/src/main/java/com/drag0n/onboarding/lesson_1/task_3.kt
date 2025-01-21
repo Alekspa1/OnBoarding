51cc865651d0aacb3da007a70d7e038180e4ea0a
DRAGON <apereverzev47@gmail.com>
2025-01-21 11:50:13 +0300

    KS-lesson-1-task-3 преобразовал часы,время полета в стринг и добавил 0 перед отображением

--- app/src/main/java/com/drag0n/onboarding/lesson_1/task_3.kt
+++ app/src/main/java/com/drag0n/onboarding/lesson_1/task_3.kt
@@ -1,6 +1,18 @@
 package com.drag0n.onboarding.lesson_1
 
 fun main() {
-    println("Тест задачи 3")
+    val yearOfSpaceFlight = 1961
+    var hourOfSpaceFlight = 9
+    var minuteOfSpaceFlight = 7
+
+    println("Год полета $yearOfSpaceFlight\n" +
+            "Час полета ${hourOfSpaceFlight.toString().map { "0$it" }.joinToString()}\n" +
+            "Минута полета ${minuteOfSpaceFlight.toString().map { "0$it" }.joinToString()}")
+
+
+    hourOfSpaceFlight = 10
+    minuteOfSpaceFlight = 55
+    println("$hourOfSpaceFlight:$minuteOfSpaceFlight")
+
 
 }
