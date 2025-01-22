9c2231bb8592c38466e40fcacc7b999ce38de108
DRAGON <apereverzev47@gmail.com>
2025-01-22 22:21:50 +0300

    KS-lesson-1-task-5 create Const

--- app/src/main/java/com/drag0n/onboarding/lesson_1/task_5.kt
+++ app/src/main/java/com/drag0n/onboarding/lesson_1/task_5.kt
@@ -2,12 +2,16 @@ package com.drag0n.onboarding.lesson_1
 
 import java.util.Locale
 
+    const val QUANTITY = 60
+    const val QUANTITY_SECOND = QUANTITY*6
+
 fun main() {
     val second = 6480
-    val hour = second/3600
-    val minute = (second % 3600) / 60
-    val secondCosmos = second % 60
+    val hour = second/ QUANTITY_SECOND
+    val minute = (second % QUANTITY_SECOND) / QUANTITY
+    val secondCosmos = second % QUANTITY
 
     println(String.format(Locale.getDefault(),
         "%02d:%02d:%02d", hour, minute, secondCosmos))
 }
+
