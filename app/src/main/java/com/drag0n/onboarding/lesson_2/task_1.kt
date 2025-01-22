9b4a1f47a8492acb505db50213b5487a575ebeaf
DRAGON <apereverzev47@gmail.com>
2025-01-22 11:52:20 +0300

    KS-lesson-2-task-1 сделал средний бал с двумя знаками после запятой

---
+++ app/src/main/java/com/drag0n/onboarding/lesson_2/task_1.kt
@@ -0,0 +1,21 @@
+package com.drag0n.onboarding.lesson_2
+
+import kotlin.math.roundToInt
+
+fun main() {
+    val averageStudentOne = 3
+    val averageStudentTwo = 4
+    val averageStudentThree = 3
+    val averageStudentFour = 5
+    println(averageGPA(averageStudentOne,averageStudentTwo,averageStudentThree,averageStudentFour))
+
+}
+fun averageGPA(
+    averageOne:Int,
+    averageTwo:Int,
+    averageThree:Int,
+    averageFour:Int): Double {
+    val result: Double = (averageOne+averageTwo+
+            averageThree+averageFour)/4.0
+    return (result*100).roundToInt()/100.0
+}
