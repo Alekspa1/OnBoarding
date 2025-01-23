3f8e1d4a323bc19e85d1ffd3fdf10a2940812a7b
DRAGON <apereverzev47@gmail.com>
2025-01-23 23:44:23 +0300

    KS-1-2 changed var on val

--- app/src/main/java/com/drag0n/onboarding/lesson_1/task_2.kt
+++ app/src/main/java/com/drag0n/onboarding/lesson_1/task_2.kt
@@ -2,7 +2,7 @@ package com.drag0n.onboarding.lesson_1
 
 fun main(){
 
-    var numberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val thankYouText: String = "Благодарим вас за покупку в нашем магазине"
 
     println(numberOfOrders)
