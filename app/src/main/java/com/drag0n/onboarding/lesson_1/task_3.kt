9886ffb8dab877638fbf4c1d4bd5ffdb394d7a2e
DRAGON <apereverzev47@gmail.com>
2025-01-24 00:04:41 +0300

    KS-1-3 made edits

---
+++ app/src/main/java/com/drag0n/onboarding/lesson_1/task_3.kt
@@ -0,0 +1,16 @@
+package com.drag0n.onboarding.lesson_1
+
+fun main() {
+    val yearOfSpaceFlight = "1961"
+    var hourOfSpaceFlight = "09"
+    var minuteOfSpaceFlight = "07"
+
+    println("Год полета $yearOfSpaceFlight\n" +
+            "Час полета $hourOfSpaceFlight\n" +
+            "Минута полета $minuteOfSpaceFlight")
+
+    hourOfSpaceFlight = "10"
+    minuteOfSpaceFlight = "55"
+    println("$hourOfSpaceFlight:$minuteOfSpaceFlight")
+
+}
