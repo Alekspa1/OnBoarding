7c3b931211c84487dd85f1450013b98e0656af3c
DRAGON <apereverzev47@gmail.com>
2025-01-25 00:42:29 +0300

    KS-1-2 made edits

---
+++ app/src/main/java/com/drag0n/onboarding/lesson_1/task_2.kt
@@ -0,0 +1,15 @@
+package com.drag0n.onboarding.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thankYouText: String = "Благодарим вас за покупку в нашем магазине"
+
+    println(numberOfOrders)
+    println(thankYouText)
+    var numberOfWorks = 2000
+    //println("Количество работников: $numberOfWorks")
+    numberOfWorks = 1999
+    println("Количество работников: $numberOfWorks")
+
+}
