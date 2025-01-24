da277b52beddea17e5bdd3e49dd51d78164ca635
DRAGON <apereverzev47@gmail.com>
2025-01-24 11:59:12 +0300

    KS-3-4 create move number

---
+++ app/src/main/java/com/drag0n/onboarding/lesson_3/task_4.kt
@@ -0,0 +1,15 @@
+package com.drag0n.onboarding.lesson_3
+
+fun main() {
+
+    var from = "E2"
+    var where = "E4"
+    var moveNumber = 0
+    println("Пришел на $where из $from, номер хода: $moveNumber")
+
+    from = "D2"
+    where = "D3"
+    moveNumber++
+    println("Пришел на $where из $from, номер хода: $moveNumber")
+
+}
