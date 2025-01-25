49b7d88e31c8f81e1d0d1846adac0aec477800af
DRAGON <apereverzev47@gmail.com>
2025-01-25 23:34:48 +0300

    KS-2-1 made edits

--- app/src/main/java/com/drag0n/onboarding/lesson_2/task_1.kt
+++ app/src/main/java/com/drag0n/onboarding/lesson_2/task_1.kt
@@ -3,12 +3,13 @@ package com.drag0n.onboarding.lesson_2
 import kotlin.math.roundToInt
 
 fun main() {
-    val averageStudentOne = 3
-    val averageStudentTwo = 4
-    val averageStudentThree = 3
-    val averageStudentFour = 5
-    val result: Double = (averageStudentOne + averageStudentTwo +
-            averageStudentThree + averageStudentFour) / 4.0
-    println((result * 100).roundToInt() / 100.0)
+    val NUMBER_OF_STUDENTS = 4
+    val englishScoreStudentOne = 3
+    val englishScoreStudentTwo = 4
+    val englishScoreStudentThree = 3
+    val englishScoreStudentFour = 5
+    val result: Float = (englishScoreStudentOne + englishScoreStudentTwo +
+            englishScoreStudentThree + englishScoreStudentFour).toFloat() / NUMBER_OF_STUDENTS
+    println(result)
 
 }
